28b9a5c48158f7997c7ae424caecf62c8c0acfd4
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-08-31 08:36:09 -0500

    feat(input): remove spellcheked from input component

--- src/shared/components/ui/input.tsx
+++ src/shared/components/ui/input.tsx
@@ -13,6 +13,6 @@ export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
  * @returns {React.ReactElement} Element.
  */
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
-  return <input ref={ref} className={cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm focus-visible:ring-2 focus-visible:ring-ring', className)} {...props} />;
+  return <input spellCheck="false" ref={ref} className={cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm focus-visible:ring-2 focus-visible:ring-ring', className)} {...props} />;
 });
 Input.displayName = 'Input';
